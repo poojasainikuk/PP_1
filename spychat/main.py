4e60b0fa7edf3393a6074e860d021e7d03356f98
poojasainikuk <dhruvikashakya16@gmail.com>
2017-09-16 20:46:52 -0700

    concatenation

---
+++ spychat/main.py
@@ -0,0 +1,5 @@
+print'Let\'s get started.... '
+spy_name = raw_input('What is your name? : ')
+spy_salutation = raw_input('what should we call you ? :')
+spy_name = spy_salutation + ' ' + spy_name
+print 'Welcome ' + spy_name + ' Glad to have you back with us.'
